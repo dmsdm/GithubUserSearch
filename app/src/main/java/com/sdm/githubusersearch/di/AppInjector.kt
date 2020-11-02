294d57836ae463a7cd5e3f764ae25f89a673b07e
Dmitry Smirnov <dmsdm@yandex-team.ru>
2020-11-02 18:06:31 +0300

    zoom avatar

--- app/src/main/java/com/sdm/githubusersearch/di/AppInjector.kt
+++ app/src/main/java/com/sdm/githubusersearch/di/AppInjector.kt
@@ -5,7 +5,6 @@ import android.app.Application
 import android.os.Bundle
 import com.sdm.githubusersearch.GithubApp
 import dagger.android.AndroidInjection
-import dagger.android.support.HasSupportFragmentInjector
 
 object AppInjector {
     fun init(githubApp: GithubApp) {
@@ -14,9 +13,7 @@ object AppInjector {
         githubApp
             .registerActivityLifecycleCallbacks(object : Application.ActivityLifecycleCallbacks {
                 override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
-                    if (activity is HasSupportFragmentInjector) {
-                        AndroidInjection.inject(activity)
-                    }
+                    AndroidInjection.inject(activity)
                 }
 
                 override fun onActivityStarted(activity: Activity) {
